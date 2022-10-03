5c56651a4a5b3e1eaa5203a1ad29f7f3ffc6b17f
Bailey Martin <bhmbob@gmail.com>
2022-10-03 20:28:08 +0100

    Updated startup to use delegate system

--- Source/Modules/AssetLibrary/Source/Scripts/startup/register_content_dirs.py
+++ Source/Modules/AssetLibrary/Source/Scripts/startup/register_content_dirs.py
@@ -1,5 +1,5 @@
 """
-:type startup
+:type script
 :desc Registers the Asset Library content directories as unreal content directories
 :desc to ensure we can map / unmap file paths
 :callbacks [startup]
@@ -13,8 +13,9 @@ import juniper.engine.types.plugin
 import unreal.juniper.content_browser
 
 
-asset_library_plugin_ref = juniper.engine.types.plugin.PluginManager().find_plugin("asset_library")
+'''asset_library_plugin_ref = juniper.engine.types.plugin.PluginManager().find_plugin("asset_library")
 unreal.juniper.content_browser.register_unreal_content_dir(
     os.path.join(asset_library_plugin_ref.root, "content"),
     "/AssetLibrary/"
 )
+'''
